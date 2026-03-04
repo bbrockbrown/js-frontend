5f178b290f81c381071f0d4d9980a31ca6cb1411
mcapriotti1 <michaelcapriotti11@gmail.com>
2026-03-04 13:53:53 -0600

    Updated Firebase Config to use env file

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -8,13 +8,13 @@ import { GoogleAuthProvider, getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDdW_jKOcqLiY4DwBE-A39Ey7cuSRNkFNQ",
-  authDomain: "rsae-fdd.firebaseapp.com",
-  projectId: "rsae-fdd",
-  storageBucket: "rsae-fdd.firebasestorage.app",
-  messagingSenderId: "105464310683",
-  appId: "1:105464310683:web:754a0409c8a40231ae8f3e",
-  measurementId: "G-XQS4R1D8EZ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
