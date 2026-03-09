a4e703d8d8d280c4cb69683b50bfcc5f0feeaeba
Maddy Young <maddogy101010@gmail.com>
2026-03-08 19:45:43 -0500

    Finished the admin dashboard with different components

--- src/common/components/dashboard/Stats.jsx
+++ src/common/components/dashboard/Stats.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
 
 import GreenVolunteers from '../../../assets/images/GreenVolunteers.png';
+import OrangeCalendar from '../../../assets/images/OrangeCalendar.png';
+import BlueClock from '../../../assets/images/BlueClock.png';
+import PurpleCheck from '../../../assets/images/PurpleCheck.png';
 
-const Stats = () => {
+const Images = {
+  Volunteers: GreenVolunteers,
+  Events: OrangeCalendar,
+  Hours: BlueClock,
+  Signups: PurpleCheck,
+}
+const colors = {
+  Volunteers: "#E3F8E9",
+  Events: "#F8F8E3",
+  Hours: "#E3F0F8",
+  Signups: "#F0DFFF",
+}
+const Stats = ({title, number, bottomText, category}) => {
   const statsDivStyle = {
     display: 'flex',
     flexDirection: 'column',
-    width: '12%',
+    justifyContent: 'space-between',
+    width: '15%',
     height: 'auto',
-    aspectRatio: '1/1',
+    aspectRatio: '4/3',
     backgroundColor: '#fff',
     borderRadius: '15px',
-    boxShadow: '0px 2px 4px gray',
+    boxShadow: '0px 2px 2px lightgray',
     padding: '20px',
+    margin: '20px',
   };
   const statsTopStyle = {
     display: 'flex',
@@ -20,34 +37,55 @@ const Stats = () => {
     justifyContent: 'space-between',
     alignItems: 'center',
     color: '#585858',
+    fontSize: '16px',
   };
   const statIconStyle = {
-    width: '50px',
-    height: 'auto',
-  };
-  const statBGStyle = {
-    height: '100%',
+    height: '40px',
     width: 'auto',
     aspectRatio: '1/1',
-    backgroundColor: '#E3F8E9',
+  };
+  const statBGStyle = {
+    height: '50px',
+    width: 'auto',
+    aspectRatio: '1/1',
+    backgroundColor: colors[category],
     borderRadius: '15px',
     display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  };
+  const statsTitleStyle = {
+    color: '#585858',
+    fontSize: '14px',
+    fontWeight: 'bold',
+    marginRight: '8px',
+  };
+  const statsNumberStyle = {
+    color: '#000',
+    fontSize: '30px',
+    fontWeight: 'bold',
+  };
+  const statsBottomTextStyle = {
+    color: '#6E6E6E',
+    fontSize: '14px',
   };
 
   return (
     <div style={statsDivStyle}>
       <div style={statsTopStyle}>
-        <div>TOTAL VOLUNTEERS</div>
+        <div style={statsTitleStyle}>{title}</div>
         <div style={statBGStyle}>
           <img
-            src={GreenVolunteers}
+            src={Images[category]}
             alt='Green Volunteers'
             style={statIconStyle}
           />
         </div>
       </div>
-      <div>6</div>
-      <div>4 active</div>
+      <div>
+      <div style={statsNumberStyle}>{number}</div>
+      <div style={statsBottomTextStyle}>{bottomText}</div>
+      </div>
     </div>
   );
 };
--- src/common/components/dashboard/UpcomingEvents.jsx
+++ src/common/components/dashboard/UpcomingEvents.jsx
@@ -1,7 +1,107 @@
 import React from 'react';
+import GrayCalendar from '../../../assets/images/GrayCalendar.png';
+import GrayClock from '../../../assets/images/GrayClock.png';
+import GrayLocation from '../../../assets/images/GrayLocation.png';
+import GrayPeople from '../../../assets/images/GrayPeople.png';
+const UpcomingEvents = ({EventTitle, tag, currVol, volCap, date, time, location}) => {
+  const tagColors = {
+    'Holiday Event': ['#C40000', '#FFCDCD'],
+    'Grocery Giveaway': ['#0A7A00', '#DEFFDA'],
+    'Other': ['#5F5F5F', '#F8F8F8'],
+  }
+  const upcomingEventDivStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    backgroundColor: '#FAFAFA',
+    borderRadius: '15px',
+    padding: '0px 20px',
+  };
+  const topUpcomingEventStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: '0px',
+    height: '40px',
+    padding: '10px 0px 0px 0px',
+  }
+  const bottomUpcomingEventStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '0px 0px 10px 0px',
+    margin: '0px',
+  }
 
-const UpcomingEvents = () => {
-  return <div>UpcomingEvents</div>;
+  const bottomUpcomingEventTextStyle = {
+    margin: '0px',
+    color: '#5F5F5F',
+    fontWeight: 'normal',
+  }
+  const titleTagStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+      alignItems: 'center',
+  }
+  const tagStyle = {
+        color: tagColors[tag][0],
+    backgroundColor: tagColors[tag][1],
+    padding: '5px 10px',
+    borderRadius: '8px',
+  }
+  const titleStyle = {
+    margin: '0px',
+    paddingRight: '10px',
+  }
+  const iconStyle = {
+    width: '20px',
+  }
+  const iconTextStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    gap: '5px',
+  }
+  const volDivStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: '5px',
+  }
+  return (
+    <div style={upcomingEventDivStyle}>
+      <div style={topUpcomingEventStyle}>
+        <div style={titleTagStyle}>
+          <h5 style={titleStyle}>{EventTitle}</h5>
+          <h5 style={tagStyle}>{tag}</h5>
+        </div>
+        <div style={volDivStyle}>
+          <img src={GrayPeople} alt="Location" style={iconStyle} />
+        <h5><span style={{color: "#5F5F5F"}}>{currVol} / </span><span style={{color: "#999999"}}>{volCap}</span></h5>
+        </div>
+      </div>
+      <div style={bottomUpcomingEventStyle}>
+        <div style={iconTextStyle}>
+          <img src={GrayCalendar} alt="Calendar" style={{width: '35px'}} />
+          <h6 style={bottomUpcomingEventTextStyle}>{date}</h6>
+        </div>
+        <div style={iconTextStyle}>
+          <img src={GrayClock} alt="Clock" style={iconStyle} />
+          <h6 style={bottomUpcomingEventTextStyle}>{time}</h6>
+        </div>
+        <div style={iconTextStyle}>
+          <img src={GrayLocation} alt="Location" style={iconStyle} />
+          <h6 style={bottomUpcomingEventTextStyle}>{location}</h6>
+        </div>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  );
 };
 
 export default UpcomingEvents;
