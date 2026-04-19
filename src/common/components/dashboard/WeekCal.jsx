473009b7228b236cd34e6b19003c11c622d95d2e
Hayley <hayleymccormack2028@u.northwestern.edu>
2026-04-19 11:19:47 -0500

    modal customized

--- src/common/components/dashboard/WeekCal.jsx
+++ src/common/components/dashboard/WeekCal.jsx
@@ -10,9 +10,64 @@ import '@schedule-x/theme-default/dist/index.css'
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { createEventModalPlugin } from '@schedule-x/event-modal'
+import Calendar from '../../../assets/images/Calendar.svg';
+import Clock from '../../../assets/images/Clock.svg';
+import { Button } from '../atoms/Button'
  
 function WeekCal() { 
     const eventsService = useState(() => createEventsServicePlugin())[0]
+
+    const eventModStyle = 
+    {
+      border: "solid, 1px, #d3d3d3",
+      borderRadius: "5px",
+      padding: "10px",
+    }
+
+    const imgStyle = 
+    {
+      width: "20px",
+    }
+
+    const eventTitle = 
+    {
+      display: 'grid',
+      alignItems: 'flex-start',
+      gridTemplateColumns: '30px 1fr',
+      padding:"2px",
+    };
+
+    const desStyle = 
+    {
+      display:"flex",
+      justifyContent:"center",
+      alignItems:"center",
+      padding: "5px",
+    }
+
+
+    const monthVector = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    const customComponents = {
+    eventModal: ({ calendarEvent }) => {
+      return (
+        <div style={eventModStyle}>
+          <div style={eventTitle}>
+            <div><img style={imgStyle} src={Calendar} alt="" /></div>
+            <div>{calendarEvent.title}</div>
+          </div>
+          <div style={eventTitle}>
+            <div><img style={imgStyle} src={Clock} alt="" /></div>
+            <div>{monthVector[calendarEvent.start.month - 1] + " " + calendarEvent.start.toString()[8] + calendarEvent.start.toString()[9] + ", " + calendarEvent.start.toString()[0] + calendarEvent.start.toString()[1] + calendarEvent.start.toString()[2] + calendarEvent.start.toString()[3]}</div>
+          </div>
+          <div style={desStyle}>{calendarEvent.description}</div>
+          <div style={desStyle}><Button.Primary>Sign Up</Button.Primary></div>
+        </div>
+      );
+    },
+  }
+
+
     const calendar = useCalendarApp(
       {
         views: [createViewWeek(), createViewMonthGrid(), createViewMonthAgenda()],
@@ -20,32 +75,23 @@ function WeekCal() {
             {
               id: '1',
               title: 'Grocery Giveaway (8/10)',
-              start: Temporal.ZonedDateTime.from('2026-03-15T02:00:00-05:00[America/Chicago]'),
-              end: Temporal.ZonedDateTime.from('2026-03-15T04:00:00-05:00[America/Chicago]'),
+              start: Temporal.ZonedDateTime.from('2026-04-15T08:00:00-05:00[America/Chicago]'),
+              end: Temporal.ZonedDateTime.from('2026-04-15T10:00:00-05:00[America/Chicago]'),
               description: 'At the corner of Church and Dodge families facing food insecurity can come pick-up a free bag of groceries for their household. '
             },
             {
               id: '2',
               title: 'Grocery Giveaway (7/10)',
-              start: Temporal.ZonedDateTime.from('2026-03-22T02:00:00-05:00[America/Chicago]'),
-              end: Temporal.ZonedDateTime.from('2026-03-22T04:00:00-05:00[America/Chicago]'),
-              description: 'At the corner of Church and Dodge families facing food insecurity can come pick-up a free bag of groceries for their household. '
-            },
-            {
-              id: '3',
-              title: 'Grocery Giveaway (5/10)',
-              start: Temporal.ZonedDateTime.from('2026-03-29T02:00:00-05:00[America/Chicago]'),
-              end: Temporal.ZonedDateTime.from('2026-03-29T04:00:00-05:00[America/Chicago]'),
-              description: 'At the corner of Church and Dodge families facing food insecurity can come pick-up a free bag of groceries for their household. '
-            },
-            {
-              id: '4',
-              title: 'Grocery Giveaway (3/10)',
-              start: Temporal.ZonedDateTime.from('2026-04-05T02:00:00-05:00[America/Chicago]'),
-              end: Temporal.ZonedDateTime.from('2026-04-05T04:00:00-05:00[America/Chicago]'),
+              start: Temporal.ZonedDateTime.from('2026-04-17T14:00:00-05:00[America/Chicago]'),
+              end: Temporal.ZonedDateTime.from('2026-04-17T17:00:00-05:00[America/Chicago]'),
               description: 'At the corner of Church and Dodge families facing food insecurity can come pick-up a free bag of groceries for their household. '
             }
           ],
+            dayBoundaries: {
+            start: '08:00',
+            end: '22:00',
+            },
+            timezone:'America/Chicago',
           plugins: [eventsService, createEventModalPlugin()]
       })
 
@@ -55,7 +101,7 @@ function WeekCal() {
   }, [])
 
   return (
-      <ScheduleXCalendar calendarApp={calendar} />
+      <ScheduleXCalendar customComponents={customComponents} calendarApp={calendar}/>
   )
 }
  
--- src/pages/admin/AdminDashBoard.jsx
+++ src/pages/admin/AdminDashBoard.jsx
@@ -4,7 +4,7 @@ import PageHeader from '@/common/components/atoms/PageHeader';
 import { BodyContainer } from '@/common/components/form/styles';
 
 import Stats from '../../common/components/dashboard/Stats';
-
+import UpcomingEvents from '@/common/components/dashboard/UpcomingEvents';
 const AdminDashBoard = () => {
   const styleBody = {
     display: 'flex',
@@ -17,7 +17,29 @@ const AdminDashBoard = () => {
     <div style={styleBody}>
       <PageHeader title='Dashboard' />
       <BodyContainer>
-        <Stats></Stats>
+        <div style={statsContainerStyle}>
+          <Stats title="TOTAL VOLUNTEERS" number="6" bottomText="4 active" category="Volunteers"/>
+          <Stats title="UPCOMING EVENTS" number="2" bottomText="Scheduled" category="Events"/>
+          <Stats title="TOTAL HOURS" number="6" bottomText="Served" category="Hours"/>
+          <Stats title="TOTAL SIGNUPS" number="7" bottomText="2 attended" category="Signups"/>
+        </div>
+        <div style={eventsVolunteerContainerStyle}>
+        <div style={eventsContainerStyle}>
+          <h3 style={styleEventsTitle}>Upcoming Events</h3>
+          <UpcomingEvents EventTitle="Spring Holiday Meal Drive" tag="Holiday Event" currVol="2" volCap="25" date="Mar 27, 2026" time="8:00 AM - 2:00 PM" location="CW Foundation Community Center"/>
+          <UpcomingEvents EventTitle="Grocery Giveaway" tag="Grocery Giveaway" currVol="0" volCap="15" date="Feb 27, 2026" time="9:00 AM - 12:00 PM" location="CW Foundation Community Center"/>
+        </div>
+        <div style={volunteerContainerStyle}>
+          <h3>Recent Volunteers</h3>
+
+          <RecentVolunteer initials="MY" name="Maddy Young" email="madeleineyoung2029@u.northwestern.edu" tag="active"/>
+          <RecentVolunteer initials="HM" name="HayleyMcCormack" email="hayleymccormack@u.northwestern.edu" tag="pending"/>
+          <RecentVolunteer initials="KX" name="Kayla Xu" email="kaylaxu@u.northwestern.edu" tag="inactive"/>
+          <RecentVolunteer initials="DA" name="Danah Ansari" email="danahansari@u.northwestern.edu" tag="active"/>
+          <RecentVolunteer initials= "AW" name="Alivia Wynn" email="aliviawynn@u.northwestern.edu" tag="pending"/>
+          <RecentVolunteer initials="BI" name="Ben Isaac" email="benisaac@u.northwestern.edu" tag="inactive"/>
+        </div>
+        </div>
       </BodyContainer>
     </div>
   );
